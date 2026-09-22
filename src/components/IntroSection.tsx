021a106c16ebb88bbe110269cc9304efd360c9e5
DavidAdedigba <daveadedigba@gmail.com>
2026-09-22 05:29:37 -0400

    hero section text for tab adjusted

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -8,12 +8,12 @@ export default function IntroSection() {
 
             <div className="size-10 rounded-full bg-black" />
 
-            <div className="md:w-4/6 flex flex-col gap-3.5 px-1.5">
+            <div className="lg:w-4/6 flex flex-col gap-3.5 px-1.5">
                 <p className="">Frontend Engineer</p>
                 <h1 className="w-full">
                     Building Interfaces Worth Talking About.
                 </h1>
-                <p className="md:w-7/11 text-[16px] md:text-[18px]">
+                <p className="md:w-9/11 text-[16px] md:text-[18px]">
                     Frontend engineer by profession, perpetual learner by circumstance. I make things for the web, explore ideas that interest me, and keep a running record of what comes out of it.
                 </p>
             </div>
